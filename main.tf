4ae7971ebc7016dd851c2986890ea5e7af343b35
Les Aker <me@lesaker.org>
2020-12-08 21:12:12 -0500

    remove dupe blocks

--- main.tf
+++ main.tf
@@ -11,38 +11,6 @@ terraform {
   }
 }
 
-
-variable "admin_email" {
-  description = "Email used for SOA record"
-  type        = string
-}
-
-variable "delegation_set_id" {
-  description = "Delegation set ID to reuse"
-  type        = string
-}
-
-variable "subzone_name" {
-  description = "Subdomain name for certificate validation zone"
-  type        = string
-}
-
-variable "cert_name" {
-  description = "Certificate name"
-  type        = string
-}
-
-variable "parent_zone_id" {
-  description = "Parent zone for delegation"
-  type        = string
-}
-
-variable "caa_records" {
-  description = "CAA records for certificate"
-  type        = list(string)
-  default     = []
-}
-
 locals {
   default_caa_records = [
     "0 iodef \"mailto:${var.admin_email}\"",
